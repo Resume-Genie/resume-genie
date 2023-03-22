ceccccea2150964e81de3b53ad5817d05706b564
SourabhRajput754 <97465158+SourabhRajput754@users.noreply.github.com>
2023-03-23 01:48:06 +0530

    add education section in template 3 (#8)

--- client/src/templates/template-3/data.js
+++ client/src/templates/template-3/data.js
@@ -1,4 +1,4 @@
-export default {
+const data = {
   info: {
     id: 1,
     name: "Sourabh Kumar Singh",
@@ -7,6 +7,18 @@ export default {
     email: "sourabh.singhdbg1513@gmail.com",
     phone_no: 7004519791,
     location: "Darbhanga,India",
-    dob:"11-12-2001",
+    dob: "11-12-2001",
+  },
+
+  education: {
+    id: 1,
+    educationName: "Graduation",
+    institutionName: 'NSHM Knowledge Campus Durgapur',
+    startingYear:"08/2019 ",
+    endingYear:" 07/2023",
+    specialization: "BTECH CSE",
+    score:"9.26/10"
   },
 };
+
+export default data;
