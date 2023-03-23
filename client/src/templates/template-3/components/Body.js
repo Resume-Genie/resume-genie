e33751a9950b1cb026fd5bdc139844035ca7c585
SourabhRajput754 <Sourabh.singhdbg1513@gmail.com>
2023-03-24 01:13:07 +0530

    Add skills and project section in template-3

--- client/src/templates/template-3/components/Body.js
+++ client/src/templates/template-3/components/Body.js
@@ -1,13 +1,21 @@
 import Education from "./Education";
+import Skills from "./Skills";
+import Others from "./Others"
 
 export default function Body({ data }) {
   return (
-    <section className="flex">
-      <section className="mt-6">
+    <section className="flex justify-between mt-6">
+      <section className="w-[560px]">
         <p className="text-[40px] font-bold ml-[102px]">EDUCATION</p>
-        <Education data={data} />
+        <Education data={data.education} />
       </section>
-      <section></section>
+      <section className="w-[600px]">
+        <Skills data={data.skills}/>
+        <section className="mt-10">
+        <Others data={data.projects} />
+      </section>
+      </section>
+      
     </section>
   );
 }
