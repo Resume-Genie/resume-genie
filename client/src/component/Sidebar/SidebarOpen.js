ed4293a46ce0f4a80113f90ade8dedcd5ea93468
Gautam Raj <63155224+Gautam25Raj@users.noreply.github.com>
2023-03-25 03:08:54 +0530

    Add New Components
    Add SideBar Closed and Open component
    
    Add Dashboard Menu Component
    
    Add new Components in Profile, Samples and Dashboard

---
+++ client/src/component/Sidebar/SidebarOpen.js
@@ -0,0 +1,75 @@
+import { useNavigate } from 'react-router-dom';
+
+import DashboardLink from '../../component/DashboardLink';
+import Button from '../../component/UI/Button';
+
+import logo from './../../assests/svg/logo.svg';
+import dashboard from './../../assests/svg/dashboard/dashboard-svg.svg';
+import library from './../../assests/svg/dashboard/library-svg.svg';
+import profile from './../../assests/svg/dashboard/profile-svg.svg';
+import logOut from './../../assests/svg/dashboard/log-out-svg.svg';
+
+const SidebarOpen = (props) => {
+  const navigate = useNavigate();
+
+  const boardLinks = [
+    {
+      logo: dashboard,
+      text: 'My Dashboard',
+      link: '/dashboard/all',
+    },
+    {
+      logo: library,
+      text: 'Sample Library',
+      link: '/dashboard/samples',
+    },
+    {
+      logo: profile,
+      text: 'My Profile',
+      link: '/dashboard/profile',
+    },
+  ];
+
+  const logoutHandler = () => {
+    localStorage.removeItem('token');
+    navigate('/login');
+  };
+
+  return (
+    <section className="w-[22vw] h-[100vh] bg-[#F1FFFE] flex flex-col items-center p-[40px]">
+      <div>
+        <img src={logo} alt="Resume Genie Logo" />
+      </div>
+
+      <Button
+        text="CREATE NEW DOCUMENT"
+        className="text-[var(--white)] text-[14px] bg-[var(--dark)] p-[18px] w-full rounded-[5px] max-w-[260px] mt-[40px] font-bold"
+        onClick={() => {
+          props.setIsOpenModel(true);
+        }}
+      />
+
+      <ul className="self-start mt-[13.4vh]">
+        {boardLinks.map((boardLink) => (
+          <li className="[&:not(:first-child)]:mt-[36px]" key={boardLink.text}>
+            <DashboardLink
+              logo={boardLink.logo}
+              text={boardLink.text}
+              link={boardLink.link}
+            />
+          </li>
+        ))}
+      </ul>
+
+      <button className="self-start mt-auto flex" onClick={logoutHandler}>
+        <div className="w-[24px] h-[24px]">
+          <img src={logOut} alt="Dashboard Logo" />
+        </div>
+
+        <p className="text-[18px] text-[var(--text)] ml-[20px]">Log Out</p>
+      </button>
+    </section>
+  );
+};
+
+export default SidebarOpen;
--- client/src/pages/Form/CreateResume.js
+++ client/src/pages/Form/CreateResume.js
@@ -1,34 +1,12 @@
+import { useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { useNavigate } from 'react-router-dom';
 
-import DashboardLink from '../../component/DashboardLink';
-
-import logoShort from './../../assests/svg/logo-short.svg';
-import dashboard from './../../assests/svg/dashboard/dashboard-svg.svg';
-import library from './../../assests/svg/dashboard/library-svg.svg';
-import profile from './../../assests/svg/dashboard/profile-svg.svg';
-import logOut from './../../assests/svg/dashboard/log-out-svg.svg';
+import SidebarClose from '../../component/Sidebar/SidebarClose';
 
 const CreateResume = () => {
-  const navigate = useNavigate();
-
-  const boardLinks = [
-    {
-      logo: dashboard,
-      text: 'MY DASHBOARD',
-      link: '/dashboard/all',
-    },
-    {
-      logo: library,
-      text: 'SAMPLE LIBRARY',
-      link: '/library',
-    },
-    {
-      logo: profile,
-      text: 'MY PROFILE',
-      link: '/profile',
-    },
-  ];
+  useEffect(() => {
+    document.title = 'Resume Genie | Create Resume';
+  }, []);
 
   const buttons = [
     'CONTACT',
@@ -40,11 +18,6 @@ const CreateResume = () => {
     'OTHERS',
   ];
 
-  const logoutHandler = () => {
-    localStorage.removeItem('token');
-    navigate('/login');
-  };
-
   return (
     <motion.section
       initial={{ opacity: 0, x: '-100%' }}
@@ -53,33 +26,7 @@ const CreateResume = () => {
       exit={{ opacity: 0, x: '100%' }}
       className="flex"
     >
-      <section className="h-[100vh] bg-[#f1fffe] flex flex-col items-center py-[40px] px-3">
-        <div>
-          <a href="/dashboard/all">
-            <img src={logoShort} alt="Resume Genie Logo" />
-          </a>
-        </div>
-
-        <ul className="mt-[15vh]">
-          {boardLinks.map((boardLink) => (
-            <li
-              className="[&:not(:first-child)]:mt-[48px] relative after:content-[attr(data-text)] tooltip tooltip-right"
-              data-text={boardLink.text}
-              key={boardLink.text}
-            >
-              <DashboardLink logo={boardLink.logo} link={boardLink.link} />
-            </li>
-          ))}
-        </ul>
-
-        <button
-          className="mt-auto w-[24px] h-[24px] relative after:content-[attr(data-text)] tooltip tooltip-right"
-          data-text="LogOut"
-        >
-          <img src={logOut} alt="Dashboard Logo" onClick={logoutHandler} />
-        </button>
-      </section>
-
+      <SidebarClose />
       <section className="p-4">
         <ul className="flex">
           {buttons.map((button) => (
