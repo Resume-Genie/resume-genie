2c8456a05b667b49b767399d33fe86b6f673d2a9
SourabhRajput754 <Sourabh.singhdbg1513@gmail.com>
2023-03-28 23:15:05 +0530

    Initial commit of Template2

---
+++ client/src/templates/template-2/components/About..js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Name = () => {
+  return <div>Name</div>;
+};
+
+export default Name;
--- client/src/templates/template-3/Template3.js
+++ client/src/templates/template-3/Template3.js
@@ -1,5 +1,5 @@
 import Navbar from "./components/Navbar";
-import data from "./data";
+import data from "../data";
 import Body from "./components/Body";
 
 export default function Template3() {
